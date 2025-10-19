e96205945a627e952bdd51c419218676cc1e78b9
SnickersDE <florian_97@live.de>
2025-10-19 12:49:11 +0200

    Delete node.js

--- node.js
+++
@@ -1,7 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_dkt1uwxg7,
-  api_key: process.env.CLOUDINARY_414614911359457,
-  api_secret: process.env.CLOUDINARY_mq7xiUOH5nKPcHGQzg1KkMbzBUs
-});
